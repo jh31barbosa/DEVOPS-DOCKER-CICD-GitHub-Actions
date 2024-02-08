ac91a1f2db66fcadb7837aa1f970f046f4fc887a
jh31barbosa <jh29.dev@gmail.com>
2024-02-08 19:22:46 -0300

    new feature

--- server.js
+++ server.js
@@ -56,10 +56,36 @@ app.post('/messages', async (req, res) => {
 })
 
 
+io.on('connection', (socket) => {
+  console.log('a user is connected');
 
-io.on('connection', () =>{
-  console.log('a user is connected')
-})
+  // Função para enviar uma mensagem automática
+  const sendAutoResponse = (message) => {
+    const autoResponses = {
+      'olá': 'Olá! Como posso ajudar?',
+      'tudo bem?': 'Tudo ótimo, e você?',
+      // Adicione mais respostas automáticas conforme necessário
+    };
+
+    const response = autoResponses[message.toLowerCase()];
+    if (response) {
+      setTimeout(() => {
+        socket.emit('message', {
+          name: 'ChatBot',
+          message: response
+        });
+      }, 1000); // Espera 1 segundo antes de enviar a resposta automática
+    }
+  };
+
+  // Evento de recebimento de uma nova mensagem
+  socket.on('message', (message) => {
+    console.log('Message received: ', message);
+    
+    // Verifica se a mensagem recebida corresponde a uma mensagem pré-definida
+    sendAutoResponse(message.message);
+  });
+});
 
 mongoose.connect(dbUrl ,{useNewUrlParser : true }, (err) => {
   console.log('mongodb connected',err);
