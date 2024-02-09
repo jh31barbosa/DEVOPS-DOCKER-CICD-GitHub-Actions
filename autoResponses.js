5ec783e5738f62a09387edbb926327f78626abfe
jh31barbosa <jh29.dev@gmail.com>
2024-02-09 05:39:52 -0300

    add more messages to autoresponse.

--- autoResponses.js
+++ autoResponses.js
@@ -1,8 +1,10 @@
 // Função para enviar uma mensagem automática
 const sendAutoResponse = (socket, message) => {
   const autoResponses = {
-    'olá': 'Olá! Como posso ajudar?',
-    'como você está?': 'Estou bem, obrigado por perguntar! E você?',
+    'ola': 'Ola! Como posso ajudar?',
+    'gostaria de tirar uma duvida?': 'Claro, qual seria sua dúvida?',
+    'gostaria de saber como posso retirar meu boleto?': 'Me informe o cpf completo, por favor',
+    '123456789': 'Encontrei seu boleto! Aqui está a fatura referente a esse mês',
     'qual é o seu nome?': 'Meu nome é ChatBot. Como posso ajudar?',
     'onde você está localizado?': 'Estou em todos os lugares onde você precisar de mim!',
     'qual é a sua função?': 'Minha função é ajudar você a tirar dúvidas e fornecer informações.',
@@ -12,7 +14,7 @@ const sendAutoResponse = (socket, message) => {
     'qual é o horário de funcionamento?': 'Nosso horário de funcionamento é das 9h às 18h, de segunda a sexta-feira.',
     'qual é o seu telefone de contato?': 'Nosso telefone de contato é (XX) XXXX-XXXX.',
     'qual é o seu endereço de e-mail?': 'Nosso endereço de e-mail é contato@empresa.com.',
-    // Adicione mais perguntas e respostas automáticas conforme necessário
+    
   };
 
   const response = autoResponses[message.toLowerCase()];
