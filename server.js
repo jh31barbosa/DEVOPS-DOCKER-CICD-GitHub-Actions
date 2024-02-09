6e00b09792cf2d053fb23a66f617b7de8970312b
jh31barbosa <jh29.dev@gmail.com>
2024-02-09 11:30:55 -0300

    add auto response

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
-//var { sendAutoResponse } = require('./autoResponses');
+var { sendAutoResponse } = require('./autoResponses');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
